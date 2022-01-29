ccd2b100a70449ef4449b279f27489c12b0453fc
Jakub Zomerfeld <jakub.zomerfeld@gmail.com>
2022-01-29 12:31:30 +0100

    feat: add prettier into project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,29 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
   },
-  'extends': [
+  extends: [
     'eslint:recommended',
+    'plugin:vue/vue3-recommended',
     'plugin:nuxt/recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
-  'parserOptions': {
-    'ecmaVersion': 13,
-    'parser': '@typescript-eslint/parser',
-    'sourceType': 'module'
+  parserOptions: {
+    ecmaVersion: 13,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
   },
-  'plugins': [
-    'vue',
-    '@typescript-eslint'
-  ],
-  'rules': {
-    'indent': ['error',2],
-    'linebreak-style': ['error','unix'],
-    'quotes': ['error','single'],
-    'semi': ['error','never'],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
     // we want to avoid extraneous spaces
+    'no-unused-vars': 'warn',
     'no-trailing-spaces': 'error',
     'no-multi-spaces': ['error'],
     'no-multiple-empty-lines': 'error'
