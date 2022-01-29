e3cab14e6c5f836f23fd41ef6f3881dd9ebfa8f3
Jakub Zomerfeld <jakub.zomerfeld@gmail.com>
2022-01-29 13:55:26 +0100

    fix: improve eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,28 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
+    'vue/setup-compiler-macros': true
   },
   extends: [
     'eslint:recommended',
+    'plugin:vue/base',
     'plugin:vue/vue3-recommended',
     'plugin:nuxt/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier'
   ],
+  // plugins: ['@typescript-eslint'],
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    ecmaVersion: 13,
+    ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
   },
-  plugins: ['@typescript-eslint'],
+  /* Check rules: https://eslint.vuejs.org/rules/ */
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
@@ -26,6 +33,9 @@ module.exports = {
     'no-unused-vars': 'warn',
     'no-trailing-spaces': 'error',
     'no-multi-spaces': ['error'],
-    'no-multiple-empty-lines': 'error'
+    'no-multiple-empty-lines': 'off',
+    'vue/no-v-html': 'off',
+    'vue/multi-word-component-names': 'warn',
+    'vue/script-setup-uses-vars': 'error' // Enable vue/script-setup-uses-vars rule
   }
 }
