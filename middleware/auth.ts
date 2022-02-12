915a217180fd0d377adb606720412c8f68181b31
J. Gonzalez <ctw@ctwhome.com>
2022-02-12 12:37:23 +0100

    Update auth.ts

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,12 +1,3 @@
-// import { isLoggedIn } from '~/api/auth'
-//
-// export default function ({ redirect }) {
-//   if (!isLoggedIn.value) {
-//     console.error('Protected Route')
-//     return redirect('/')
-//   }
-// }
-
 export default defineNuxtRouteMiddleware((to, from) => {
   const { isLoggedIn } = useAuth()
   if (!isLoggedIn()) {
