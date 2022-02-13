d9d25dc9f1b0e72648967bd978f14f26dbee3612
Jesús García <ctw@ctwhome.com>
2022-02-13 10:00:03 +0100

    wip: supabase

--- plugins/supabase.client.ts
+++ plugins/supabase.client.ts
@@ -3,11 +3,11 @@
  */
 
 import {defineNuxtPlugin} from '#app'
-import { createClient } from '@supabase/supabase-js'
+// import { createClient } from '@supabase/supabase-js'
 
 export default defineNuxtPlugin(nuxtApp => {
-  const config = useRuntimeConfig()
-  const supabase = createClient(config.supabaseUrl, config.supabasePublicKey)
-  nuxtApp.provide('supabase',supabase)
-  return supabase
+  // const config = useRuntimeConfig()
+  // const supabase = createClient(config.supabaseUrl, config.supabasePublicKey)
+  // nuxtApp.provide('supabase',supabase)
+  // return supabase
 })
