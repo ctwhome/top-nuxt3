f4ec504526790844107ae2620f5b146804b116e3
Jesús García <ctw@ctwhome.com>
2022-02-14 19:27:33 +0100

    wip: env for composables

--- composables/useSupabase.ts
+++ composables/useSupabase.ts
@@ -1,11 +1,14 @@
+import { useRuntimeConfig } from '#app'
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://uduilifzyiujjrzlwyby.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTIzNzkyMywiZXhwIjoxOTMwODEzOTIzfQ.morp5e9629xzWqRwcy1d6J3FbGbQKSwOyOAoK1URv1I'
+// const SUPABASE_URL = 'https://uduilifzyiujjrzlwyby.supabase.co'
+// const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTIzNzkyMywiZXhwIjoxOTMwODEzOTIzfQ.morp5e9629xzWqRwcy1d6J3FbGbQKSwOyOAoK1URv1I'
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
-
-
+let supabase
 export default function useSupabase () {
+  // supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+  console.log('🎹 useRuntimeConfig()', useRuntimeConfig())
+
+  supabase = supabase || createClient(useRuntimeConfig().SUPABASE_URL, useRuntimeConfig().SUPABASE_PUBLIC_KEY)
   return { supabase }
 }
